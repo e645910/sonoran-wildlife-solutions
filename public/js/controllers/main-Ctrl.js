c339ad9d5d36437d40cda7c8c90fbe2714a19d83
rwightman <rick.wightman@q.com>
2016-02-19 19:47:32 -0700

    changed dropdowns to bootstrap

--- public/js/controllers/main-Ctrl.js
+++ public/js/controllers/main-Ctrl.js
@@ -1,12 +1,33 @@
 angular.module('sws')
-    .controller('mainController', function($scope) {
+    .controller('mainController', function($scope, $log) {
         $scope.myInterval = 5000;
         $scope.slides = [
-        {image: '/img/don1.jpg'},
-        {image: '/img/don2.jpg'},
-        {image: '/img/don3.jpg'},
-        {image: '/img/don4.jpg'},
-        {image: '/img/don5.jpg'},
-        {image: '/img/don6.jpg'}
+            { image: '/img/don1.jpg' },
+            { image: '/img/don2.jpg' },
+            { image: '/img/don3.jpg' },
+            { image: '/img/don4.jpg' },
+            { image: '/img/don5.jpg' },
+            { image: '/img/don6.jpg' }
         ];
+
+        $scope.items = [
+            'The first choice!',
+            'And another choice for you.',
+            'but wait! A third!'
+        ];
+
+        $scope.status = {
+            isopen: false
+        };
+
+        $scope.toggled = function(open) {
+            $log.log('Dropdown is now: ', open);
+        };
+
+        $scope.toggleDropdown = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            $scope.status.isopen = !$scope.status.isopen;
+        };
+
     });

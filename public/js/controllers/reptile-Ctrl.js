ea530acfff9b98cac920e849fd90f0a88226ef3c
rwightman <rick.wightman@q.com>
2016-02-24 13:05:40 -0700

    reduced main image size

--- public/js/controllers/reptile-Ctrl.js
+++ public/js/controllers/reptile-Ctrl.js
@@ -1,12 +1,12 @@
 angular.module('sws')
     .controller('reptileController', function($scope, $log) {
-        // $scope.myInterval = 5000;
+        $scope.myInterval = 5000;
         $scope.slides = [
-            { image: '/img/reptiles/snake1.jpg' },
-            { image: '/img/reptiles/snake2.jpg' },
-            { image: '/img/reptiles/snake3.jpg' },
-            { image: '/img/reptiles/snake4.jpg' },
-            { image: '/img/reptiles/snake5.jpg' },
-            { image: '/img/reptiles/snake6.jpg' }
+            { image: '/img/snake1.jpg' },
+            { image: '/img/snake2.jpg' },
+            { image: '/img/snake3.jpg' },
+            { image: '/img/snake4.jpg' },
+            { image: '/img/snake5.jpg' },
+            { image: '/img/snake6.jpg' }
         ];
     });

a503d20b2dd9372ff7cf58e61d4920b1a2288d0f
rwightman <rick.wightman@q.com>
2016-02-24 19:51:13 -0700

    made carousel image fit all containers

--- public/js/controllers/reptile-Ctrl.js
+++ public/js/controllers/reptile-Ctrl.js
@@ -1,12 +1,14 @@
 angular.module('sws')
-    .controller('reptileController', function($scope, $log) {
+    .controller('reptileController', function($scope, $location, $log) {
         $scope.myInterval = 5000;
+        $scope.w = window.innerWidth;
+        $scope.h = window.innerHeight - 80;
         $scope.slides = [
             { image: '/img/snake1.jpg' },
             { image: '/img/snake2.jpg' },
-            { image: '/img/snake3.jpg' },
-            { image: '/img/snake4.jpg' },
-            { image: '/img/snake5.jpg' },
-            { image: '/img/snake6.jpg' }
+            // { image: '/img/snake3.jpg' },
+            // { image: '/img/snake4.jpg' },
+            // { image: '/img/snake5.jpg' },
+            // { image: '/img/snake6.jpg' }
         ];
     });

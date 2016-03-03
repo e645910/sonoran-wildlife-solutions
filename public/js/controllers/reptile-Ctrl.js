3917a677d5ee708baae3ce0adb9d6d8e57eecee8
rwightman <rick.wightman@q.com>
2016-03-03 14:09:07 -0700

    testing the main image load time

--- public/js/controllers/reptile-Ctrl.js
+++ public/js/controllers/reptile-Ctrl.js
@@ -2,10 +2,10 @@ angular.module('sws')
     .controller('reptileController', function($scope, $location, $log) {
         $scope.myIntervals = 9000;
         $scope.slides = [
-            { image: '/img/snake1.jpg', name: 'Mojave rattlesnake.', text: 'They are both aggressive and fast--a deadly combination.'},
-            { image: '/img/snake2.jpg', name: 'Black Tail Rattlesnake', text: 'The Black-tailed is generally considered one of the less aggressive rattlesnakes, but is still a dangerous snake.'},
+            { image: '/img/snake1.jpg', name: 'Mojave Rattlesnake.', text: 'They are both aggressive and fast--a deadly combination.'},
+            { image: '/img/snake2.jpg', name: 'Black-Tail Rattlesnake', text: 'The Black-tailed is generally considered one of the less aggressive rattlesnakes, but is still a dangerous snake.'},
             { image: '/img/snake3.jpg', name: 'Western Dimondback Rattlesnake', text: 'The Dimondback is venomous and is responsible for the second greatest number of snakebite fatalities in the u.s..'},
-            { image: '/img/Gila Monster.jpg', name: 'Gila Monster', text: 'Gila Monsters have a venomous bite.  Generally speaking they are slow moving.  However, they are capable of quick movement.' },
-            { image: '/img/coloradorivertoad.jpg', name: 'Colorado Toad', text: 'When a Colorado River toad is threatened, it will secrete a milky-white hallucinogenic toxin.  Dogs that have attacked toads have been paralyzed or even killed.' }
+            { image: '/img/Gila Monster.jpg', name: 'Gila Monster', text: 'Gila Monsters have a venomous bite.  generally speaking they are slow moving.  however, they are capable of quick movement.' },
+            { image: '/img/coloradorivertoad.jpg', name: 'Colorado River Toad', text: 'When a Colorado River toad is threatened, it will secrete a milky-white toxin.  dogs that have attacked toads have been paralyzed or even killed.' }
         ];
     });

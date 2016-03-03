f38fa38503a624db908542814ca9c0bb1c23c95a
rwightman <rick.wightman@q.com>
2016-03-03 13:42:09 -0700

    testing the main image load time

--- public/js/controllers/reptile-Ctrl.js
+++ public/js/controllers/reptile-Ctrl.js
@@ -2,7 +2,7 @@ angular.module('sws')
     .controller('reptileController', function($scope, $location, $log) {
         $scope.myIntervals = 9000;
         $scope.slides = [
-            { image: '/img/snake1.jpg', name: 'This is a Mojave rattlesnake.', text: 'They are both aggressive and fast--a deadly combination.'},
+            { image: '/img/snake1.jpg', name: 'Mojave rattlesnake.', text: 'They are both aggressive and fast--a deadly combination.'},
             { image: '/img/snake2.jpg', name: 'Black Tail Rattlesnake', text: 'The Black-tailed is generally considered one of the less aggressive rattlesnakes, but is still a dangerous snake.'},
             { image: '/img/snake3.jpg', name: 'Western Dimondback Rattlesnake', text: 'The Dimondback is venomous and is responsible for the second greatest number of snakebite fatalities in the u.s..'},
             { image: '/img/Gila Monster.jpg', name: 'Gila Monster', text: 'Gila Monsters have a venomous bite.  Generally speaking they are slow moving.  However, they are capable of quick movement.' },

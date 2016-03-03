6fcb652b1242af677bc10eb343eaf94a911d3672
rwightman <rick.wightman@q.com>
2016-03-03 14:03:17 -0700

    testing the main image load time

--- public/js/controllers/reptile-Ctrl.js
+++ public/js/controllers/reptile-Ctrl.js
@@ -6,6 +6,6 @@ angular.module('sws')
             { image: '/img/snake2.jpg', name: 'Black Tail Rattlesnake', text: 'The Black-tailed is generally considered one of the less aggressive rattlesnakes, but is still a dangerous snake.'},
             { image: '/img/snake3.jpg', name: 'Western Dimondback Rattlesnake', text: 'The Dimondback is venomous and is responsible for the second greatest number of snakebite fatalities in the u.s..'},
             { image: '/img/Gila Monster.jpg', name: 'Gila Monster', text: 'Gila Monsters have a venomous bite.  Generally speaking they are slow moving.  However, they are capable of quick movement.' },
-            { image: '/img/coloradorivertoad.jpg', name: 'colorado Toad', text: 'When a Colorado River toad is threatened, it will secrete a milky-white hallucinogenic toxin.  Dogs that have attacked toads have been paralyzed or even killed.' }
+            { image: '/img/coloradorivertoad.jpg', name: 'Colorado Toad', text: 'When a Colorado River toad is threatened, it will secrete a milky-white hallucinogenic toxin.  Dogs that have attacked toads have been paralyzed or even killed.' }
         ];
     });

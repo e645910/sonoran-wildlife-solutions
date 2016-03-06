6b2370718d17bdeedfac1b9c186b7c8b23860daf
rwightman <rick.wightman@q.com>
2016-03-05 21:22:43 -0700

    changed icon and added pitures to rodents

--- public/js/controllers/raccoons-Ctrl.js
+++ public/js/controllers/raccoons-Ctrl.js
@@ -5,7 +5,7 @@ angular.module('sws')
             { image: '/img/raccoons/ringtail.jpg', name: 'Ringtail Cat', text: "Ringtail cats are arizona's state mammal.  they are curious, and they look so cute that you might be tempted to pet them...but don't!  their bite is worse than their bark."},
             { image: '/img/raccoons/raccoon.jpg', name: 'Raccoon', text: 'Raccoons like dark elevated holes.  they also like water features because they are fond of washing their food.  in urban areas they like to take up residence in attics and chimneys.'},
             { image: '/img/raccoons/coatis.jpg', name: 'Coatis', text: 'Coatis rarely take up residence in houses and structures, but they do sometimes dig up gardens and landscaping looking for grubs and fruits.'},
-            { image: '/img/raccoons/spottedSkunk.jpg', name: 'Spotted Skunk', text: 'Skunks are critters that need to be handled with care for very obvious reasons.  their primary mission in life is finding food. '},
+            { image: '/img/raccoons/spottedSkunk.jpg', name: 'Spotted Skunk', text: 'Skunks are critters that need to be handled with care for very obvious reasons.'},
             { image: '/img/raccoons/stripedSkunk.jpg', name: 'Striped Skunk', text: 'Skunks are omnivores and will eat just about anything.  generally speaking, skunks only spray when they have an altercation with another animal, or for some other reason feel threatened.'}
         ];
     });
--- public/js/controllers/rodents-Ctrl.js
+++ public/js/controllers/rodents-Ctrl.js
@@ -2,10 +2,10 @@ angular.module('sws')
     .controller('rodentsController', function($scope) {
         $scope.myIntervals = 9000;
         $scope.slides = [
-            { image: '/img/rodents/townsend.jpg', name: 'Townsends Big Eared', text: "This townsend's big eared bat lives with others of his kind in the soil-piping features of the cienega creek preserve southeast of Tucson."},
-            { image: '/img/rodents/canyon.jpg', name: 'Canyon Bat or Western Pipistrelle', text: "Arizona's smallest bat, they are notorious for squeezing into very small cracks and crevices along the eaves of houses."},
-            { image: '/img/rodents/mexican.jpg', name: 'Mexican Long-Tongued Bat', text: "Some of arizona's bats are nectar feeders and can be seen hitting humming bird feeders at night."},
-            { image: '/img/rodents/brown.jpg', name: 'Big Brown Bats', text: 'Big brown bats tend to cluster in larger colonies and like to occupy houses and structures--especially attics. ' },
-            { image: '/img/rodents/pocketed.jpg', name: 'Pocketed Freetail Bat', text: 'Bats are not flying rodents; they are delicate mammals.  their wings are actually their hands.'}
+            { image: '/img/rodents/packRat.jpg', name: 'Packrat', text: 'Packrats not only gnaw continually, but are notorious collectors of things, all of which they drag into their nests.', subText: 'Kissing bugs often breed in packrat nests.  when they bite you, they suck blood and leave a nasty bite that itches and often leaves a large red welt.'},
+            { image: '/img/rodents/harris.jpg', name: 'Harris Antelope Ground Squirrel', text: "These guys like to eat seeds and cactus fruits, when they get into gardens they will eat your plants, but usually don't kill them"},
+            { image: '/img/rodents/rockSquirrel.jpg', name: 'Rock Squirrel', text: 'They can be very destructive and like to tunnel under houses and other structures leaving the foundations weakened.  they can also chew up irrigation lines.'},
+            { image: '/img/rodents/pocket.jpg', name: 'Pocket Mouse', text: 'Rats and Mice gnaw and grind on things day in and day out to keep their teeth worn down.  that means that packrat control and rodent control are constant issue for homeowners.' },
+            { image: '/img/rodents/roundTail.jpg', name: 'Round-tailed Squirrel', text: 'Some people call these little critters prairie dogs, but they are not.  watch out for them as they will get into your gardens too'}
         ];
     });
